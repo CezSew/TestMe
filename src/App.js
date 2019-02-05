dab71213abee1897052e1454a06d6f315f29da3a
CezSew <csewerynski@interia.pl>
2019-02-05 22:51:42 +0100

    Setting up the basics

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Button from './Button';
+import Title from './Title';
 import './App.css';
 
 class App extends Component {
-
+  constructor(props) {
+    super(props);
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+  }
+  handleButtonClick = () => {
+    console.log('click');
+  }   
+  
   render() {
     return (
       <div className="App">
         <section className="container d-flex flex-column justify-content-center align-items-center">
-          <h1 className="title text-center text-muted mb-5">TestMe</h1>
-          <button className="btn btn-primary">ZACZNIJ TEST</button>
+          <Title text="TestMe"/>
+          <Button handleClick={this.handleButtonClick}/>
         </section>
       </div>
     );
---
+++ src/Title.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+const Title = ({text}) => (
+    <h1 className="title text-center text-muted mb-5">
+      {text}
+    </h1>
+);
+
+export default Title;
