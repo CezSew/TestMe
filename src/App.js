485c4da05cbb9ceef122ef11e8b078d6df2cbd7f
CezSew <csewerynski@interia.pl>
2019-02-05 22:21:22 +0100

    My first commit

--- src/App.js
+++ src/App.js
@@ -3,23 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <section className="container d-flex flex-column justify-content-center align-items-center">
+          <h1 className="title text-center text-muted mb-5">TestMe</h1>
+          <button className="btn btn-primary">ZACZNIJ TEST</button>
+        </section>
       </div>
     );
   }
